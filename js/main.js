08392b7154b5ba3d277433bffe9b1556dbd76972
Juan Pablo Favale <juanpablofavale@hotmail.com>
2022-09-25 02:37:10 -0300

    formularios listos en pagina contactenos

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 var button = document.querySelector('.header__button');
 var nav = document.querySelector('.header__nav');
+var btnLogin = document.querySelector("#login");
+var btnRegister = document.querySelector("#register");
+var frmLogin = document.querySelector(".form")
+
 button.addEventListener('click', function () {
     nav.classList.toggle('header__nav--activo');
+});
+
+btnLogin.addEventListener("click", function () {
+    frmLogin.classList.toggle("login");
+});
+
+btnRegister.addEventListener("click", function(){
+    frmLogin.classList.toggle("register");
 });
