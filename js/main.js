4c36f166c00a82a202f51057a1119e9b6bfc6771
Juan Pablo Favale <juanpablofavale@hotmail.com>
2022-09-25 22:50:09 -0300

    tema oscuro y variables para los colores

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ var button = document.querySelector('.header__button');
 var nav = document.querySelector('.header__nav');
 var btnLogin = document.querySelector("#login");
 var btnRegister = document.querySelector("#register");
-var frmLogin = document.querySelector(".form")
+var frmLogin = document.querySelector(".form");
+var btnTema = document.querySelector("#tema");
+var cuerpo = document.querySelector("body");
 
 button.addEventListener('click', function () {
     nav.classList.toggle('header__nav--activo');
@@ -12,6 +14,21 @@ btnLogin.addEventListener("click", function () {
     frmLogin.classList.toggle("login");
 });
 
-btnRegister.addEventListener("click", function(){
+btnRegister.addEventListener("click", function () {
     frmLogin.classList.toggle("register");
+});
+
+function cambiarModo() {
+    var oscuro = cuerpo.classList.toggle("dark");
+    localStorage.setItem("oscuro", oscuro);
+}
+
+btnTema.addEventListener("click", function () {
+    cambiarModo();
+})
+
+document.addEventListener("DOMContentLoaded", function () {
+    if (localStorage.getItem("oscuro")=="true") {
+        cambiarModo();
+    }
 });
