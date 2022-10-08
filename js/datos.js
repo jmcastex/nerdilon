93051f8f254c9f82794106649ace9bcc25302230
Juan Pablo Favale <juanpablofavale@hotmail.com>
2022-10-08 20:25:56 -0300

    mas productos

--- js/datos.js
+++ js/datos.js
@@ -1,20 +1,22 @@
-let url="../resources/data/productos.json"
+let url = "../resources/data/productos.json"
 
-fetch(url, {mode:"no-cors"})
+fetch(url, { mode: "no-cors" })
     .then(res => res.json())
     .then(data => {
         let cadena = "";
 
-        for (let prod of data){
-            cadena += `
+        for (let tipo in data) {
+            for (let prod of data[tipo]) {
+                cadena += `
                 <div class="card_tienda">
                     <img src="${prod.imagen}" alt="${prod.nombre}">
                     <h4>${prod.nombre}</h4>
                     <p>$${prod.precio}</p>
                     <a href="#">Agregar al carrito</a>
-                </div>
-                `
+                    </div>
+                    `
+            }
         }
 
-        document.getElementById("productosTienda").innerHTML=cadena
+        document.getElementById("productosTienda").innerHTML = cadena
     })
