5b01e1cbd1921c054681018d5aee05de09e25e07
Juan Pablo Favale <juanpablofavale@hotmail.com>
2022-10-09 00:05:26 -0300

    filtros aplicados y responsive a la tienda

--- js/datos.js
+++ js/datos.js
@@ -1,22 +1,38 @@
 let url = "../resources/data/productos.json"
 
-fetch(url, { mode: "no-cors" })
-    .then(res => res.json())
-    .then(data => {
-        let cadena = "";
+let filtros = {
+    tazas: document.getElementById("tazas"),
+    remeras: document.getElementById("remeras"),
+    llaveros: document.getElementById("llaveros"),
+    muniecos: document.getElementById("muniecos"),
+    mousepads: document.getElementById("mousepads"),
+}
 
-        for (let tipo in data) {
-            for (let prod of data[tipo]) {
-                cadena += `
-                <div class="card_tienda">
-                    <img src="${prod.imagen}" alt="${prod.nombre}">
-                    <h4>${prod.nombre}</h4>
-                    <p>$${prod.precio}</p>
-                    <a href="#">Agregar al carrito</a>
-                    </div>
-                    `
+
+function pintarDatos(){
+    fetch(url, { mode: "no-cors" })
+        .then(res => res.json())
+        .then(data => {
+            let cadena = "";
+    
+            for (let tipo in data) {
+                if (!filtros[tipo].checked){
+                //    continue;
+                }else{
+                    for (let prod of data[tipo]) {
+                        cadena += `
+                        <div class="card_tienda">
+                        <img src="${prod.imagen}" alt="${prod.nombre}">
+                        <h4>${prod.nombre}</h4>
+                        <p>$${prod.precio}</p>
+                        <a href="#">Agregar al carrito</a>
+                        </div>
+                        `
+                    }
+                }
             }
-        }
+    
+            document.getElementById("productosTienda").innerHTML = cadena
+        })
+}
 
-        document.getElementById("productosTienda").innerHTML = cadena
-    })
